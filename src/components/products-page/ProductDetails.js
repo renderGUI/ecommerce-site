cf8bd6df64c3a2547472f680dccf6ddd3ab99e65
renderGUI <90111307+renderGUI@users.noreply.github.com>
2022-06-04 11:15:00 -0400

    Render product details

--- src/components/products-page/ProductDetails.js
+++ src/components/products-page/ProductDetails.js
@@ -1,11 +1,30 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
   const params = useParams();
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3001/items/${params.productId}`
+        );
+        const data = await response.json();
+        setProduct(data);
+      } catch {
+        throw new Error("Something went wrong!");
+      }
+    };
+
+    fetchProduct();
+  }, [params.productId]);
 
   return (
     <div>
-      <p>Product details about product number {params.productId}</p>
+      <h2>{product.item}</h2>
+      <p>{product.price}</p>
     </div>
   );
 };
