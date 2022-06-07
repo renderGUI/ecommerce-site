1622299f0d39faa1b70628b6b4522496661ca8f1
renderGUI <90111307+renderGUI@users.noreply.github.com>
2022-06-07 07:30:30 -0400

    Add 404 page content

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,7 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 const ErrorPage = () => {
+  const navigate = useNavigate();
+  const navigateHomeHandler = () => {
+    navigate("/");
+  };
   return (
     <div>
-      <h1>ERROR PAGE</h1>
+      <h2>OOPS!</h2>
+      <p>404 - PAGE NOT FOUND!</p>
+      <p>
+        The page you are looking for might have been removed or doesn't exist.
+      </p>
+      <button type="button" onClick={navigateHomeHandler}>
+        GO TO HOMEPAGE
+      </button>
     </div>
   );
 };
